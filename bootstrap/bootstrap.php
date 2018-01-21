032851c2c77d7a48f289f1b96ce6b9fad085d797
Alihuseyn <alihuseyn13@gmail.com>
2018-01-21 22:19:14 +0300

    Remove library not required

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -4,10 +4,6 @@
 * Initialize 3rd Party libraries
 * ---------------------------------------
 */
-// Load Enviroment 
-$dotenv = new Dotenv\Dotenv(dirname(__DIR__));
-$dotenv->load();
-
 // Set Default Timezone & Locale
 setlocale(LC_TIME, 'tr_TR.utf8');
 date_default_timezone_set('Europe/Istanbul');
