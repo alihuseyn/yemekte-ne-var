23f2352d8940432694a2402e05df262e80e1898b
Alihuseyn <alihuseyn13@gmail.com>
2018-01-24 22:17:25 +0200

    Style Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,5 @@
 <?php
 // Load All Required Libraries and Modules and Arrange Namespaces
-require(dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php');
+require dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 // Initialize 3rd party libraries inside this file
-require(__DIR__.DIRECTORY_SEPARATOR.'bootstrap.php');
+require __DIR__.DIRECTORY_SEPARATOR.'bootstrap.php';
--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 /**
-* ---------------------------------------
-* Initialize 3rd Party libraries
-* ---------------------------------------
-*/
+ * ---------------------------------------
+ * Initialize 3rd Party libraries
+ * ---------------------------------------
+ */
 // Load Enviroment 
 $dotenv = new Dotenv\Dotenv(dirname(__DIR__));
 if (getenv('APP_ENV') === 'development') {
-  $dotenv->load();
+	$dotenv->load();
 }
 
 // Set Default Timezone & Locale
@@ -17,6 +17,6 @@ date_default_timezone_set('Europe/Istanbul');
 // Initialize Router
 $app = new \Klein\Klein();
 // Require router file for routes
-require(dirname(__DIR__).DIRECTORY_SEPARATOR.'route'.DIRECTORY_SEPARATOR.'routes.php');
+require dirname(__DIR__).DIRECTORY_SEPARATOR.'route'.DIRECTORY_SEPARATOR.'routes.php';
 // Dispatch Router
 $app->dispatch();
